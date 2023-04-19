ad14d1fe8da7451096e5c55703f5eef7b85bdc18
yogesh-naidu <yogeshnaidu33@gmail.com>
2023-04-18 19:50:54 -0500

    created giraffe pug file

--- routes/giraffe.js
+++ routes/giraffe.js
@@ -6,4 +6,8 @@ var router = express.Router();
 router.get('/', giraffe_controlers.giraffe_view_all_Page);
 /* GET detail giraffe page */
 router.get('/detail', giraffe_controlers.giraffe_view_one_Page);
+/* GET create giraffe page */
+router.get('/create', giraffe_controlers.giraffe_create_Page);
+/* GET create update page */
+router.get('/update', giraffe_controlers.giraffe_update_Page);
 module.exports = router;
