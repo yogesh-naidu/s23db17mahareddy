d35871f20ee1256ceb07fbf827ae7ad2327f31c7
yogesh-naidu <yogeshnaidu33@gmail.com>
2023-04-23 22:13:06 -0500

    updated all

--- models/giraffe.js
+++ models/giraffe.js
@@ -1,8 +1,21 @@
 const mongoose = require("mongoose")
 const giraffeSchema = mongoose.Schema({
-    giraffe_name: String,
-    giraffe_color: String,
-    giraffe_weight: Number
+    giraffe_name: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: [7, "Name of the Giraffe is not correct"],
+      },
+    giraffe_color: {
+        type: String,
+        required: true,
+        minLength: [2, "Color of the giraffe is not correct"],
+      },
+    giraffe_weight: {
+        type: Number,
+        required: true,
+        min: [100, "Weight of the Giraffe is not correct"],
+      }
 })
 module.exports = mongoose.model("giraffe",
 giraffeSchema)
--- routes/giraffe.js
+++ routes/giraffe.js
@@ -1,6 +1,17 @@
 var express = require('express');
-const giraffe_controlers= require('../controllers/giraffe');
+const giraffe_controlers = require('../controllers/giraffe');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 
 /* GET giraffe */
 router.get('/', giraffe_controlers.giraffe_view_all_Page);
@@ -9,7 +20,7 @@ router.get('/detail', giraffe_controlers.giraffe_view_one_Page);
 /* GET create giraffe page */
 router.get('/create', giraffe_controlers.giraffe_create_Page);
 /* GET create update page */
-router.get('/update', giraffe_controlers.giraffe_update_Page);
+router.get('/update', secured, giraffe_controlers.giraffe_update_Page);
 /* GET delete giraffe page */
 router.get('/delete', giraffe_controlers.giraffe_delete_Page);
 module.exports = router;
